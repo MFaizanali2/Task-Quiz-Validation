e28b17e40865920cd8427ffe642f4efa0522e090
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-18 23:32:23 +0500

    just edit

--- signup/signup.js
+++ signup/signup.js
@@ -1,7 +1,7 @@
 let formfield = document.querySelectorAll("form input")
 
 console.log(formfield)
-const [Username, UserEmail, UserPassword] = formfield;
+const [Username, UserEmail, UserPassword, Userpic] = formfield;
 
 console.log(Username, UserEmail, UserPassword)
 // let arraydata = [];
@@ -11,18 +11,19 @@ function signup(){
     event.preventDefault();
     
     // window.location.href = "../Login/login.html";
-    if(Username.value !== "" && UserEmail.value !== "" && UserPassword.value !== ""){
+    if(Username.value !== "" && UserEmail.value !== "" && UserPassword.value !== "" && Userpic !== ""){
         let obj = {
             user : Username.value,
             signupEmail : UserEmail.value,
             signupPassword : UserPassword.value,
+            pic : Userpic.value,
         }
         localStorage.setItem("userData", JSON.stringify(obj))
         // console.log(window.location.href = "../Login/login.html");
         window.location.href = "../Login/login.html";
         // arraydata.push(obj)
         // console.log(arraydata)
-    console.log(Username.value, UserEmail.value, UserPassword.value)
+    console.log(Username.value, UserEmail.value, UserPassword.value, Userpic.value)
     }else{
         alert("pillay")
     }
