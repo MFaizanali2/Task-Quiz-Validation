be6cc392ae42227f3d640e8606eb3908cbab2c3e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-18 23:20:13 +0500

    start work

---
+++ Login/login.js
@@ -0,0 +1,6 @@
+let formfield = document.querySelectorAll("form input")
+
+console.log(formfield)
+const [Username, UserEmail, UserPassword] = formfield;
+
+console.log(Username, UserEmail, UserPassword)
