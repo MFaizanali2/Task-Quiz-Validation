e5b526ec28594afccb9abf0ba525164a682137a4
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-18 21:06:22 +0500

    console start
    continue

--- signup/app.js
+++ signup/app.js
@@ -1,4 +1,5 @@
-
+let formfield = document.querySelector("form")
+console.log(formfield)
 
 
 
@@ -6,5 +7,5 @@
 function login(){
     // console.log("hello world")
     // window.location.href = '../Log-in/index.html'
-    
+
 }
