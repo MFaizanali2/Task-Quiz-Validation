d108a2ec3cf7a69a86f14e47c64b51d63cb7baed
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-18 23:22:03 +0500

    update line 4
    continue

--- Login/login.js
+++ Login/login.js
@@ -1,6 +1,6 @@
 let formfield = document.querySelectorAll("form input")
 
 console.log(formfield)
-const [Username, UserEmail, UserPassword] = formfield;
+const [loginEmail, loginPassword,] = formfield;
 
 console.log(Username, UserEmail, UserPassword)
