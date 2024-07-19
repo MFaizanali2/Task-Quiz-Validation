1c009ec3a626ff6456b03824efc87b8bbe3fc95e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-19 17:12:38 +0500

    add function

--- Login/login.js
+++ Login/login.js
@@ -3,4 +3,9 @@ let formfield = document.querySelectorAll("form input")
 console.log(formfield)
 const [loginEmail, loginPassword,] = formfield;
 
-console.log(Username, UserEmail, UserPassword)
+
+const login = () => {
+    
+}
+
+console.log(loginEmail, loginPassword)
