e1a8a5b9950d123c952bcb3677d21f6860af3652
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-19 17:52:26 +0500

    js line 10

--- Login/login.js
+++ Login/login.js
@@ -3,11 +3,14 @@ let formfield = document.querySelectorAll("form input")
 console.log(formfield)
 const [loginEmail, loginPassword] = formfield;
 
-let data = localStorage.getItem("userData")
+let data = JSON.parse(localStorage.getItem("userData"))
 
 const login = () => {
      
+    [UserEmail, UserPassword] = data;
     event.preventDefault();
     console.log(loginEmail.value , loginPassword.value)
+    console.log(data)
+    window.location.href = "../dashboard/dash.html";
 
 }
