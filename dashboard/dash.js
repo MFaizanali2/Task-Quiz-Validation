5dbe944789b7b88f14e3f5a8923690d6ef08fa27
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-24 17:44:36 +0500

    Update dash.js

--- dashboard/dash.js
+++ dashboard/dash.js
@@ -1,5 +1,6 @@
 let data = JSON.parse(localStorage.getItem("userData"))
 let Usename = document.getElementById("name")
+let images = document.getElementById("img")
 
 const showdata = () =>{
     const {user, pic} = data
